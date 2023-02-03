c484c192d1a414c73abc1c107547e8f8e6b06ac1
Olga Felczak <ofelczak@gmail.com>
2023-02-03 16:22:59 +0000

    Data from api is now displayed in the ui

--- assets/js/ApisCalls.js
+++ assets/js/ApisCalls.js
@@ -7,8 +7,24 @@ async function makeCalls() {
     resGeoApi.data[0].countryCode
   );
 
+  // Name of country
+  const countryName = $("<p>").text(`Country: ${resCountryApi[0].name.common}`);
+  // flag
+  const flagOfCountry = $("<p>").text(`Flag: ${resCountryApi[0].flag}`);
+  // language
+  const lng = Object.keys(resCountryApi[0].languages)[0];
+  const languageOfCountry = $("<p>").text(
+    `Language: ${resCountryApi[0].languages[lng]}`
+  );
+  // currency
+  const curr = Object.keys(resCountryApi[0].currencies)[0];
+  const currencyOfCountry = $("<p>").text(`Currency: ${curr}`);
+
   const pageId = Object.keys(resWikiApi.query.pages);
-  $("#info").append(resWikiApi.query.pages[pageId].extract);
+  $("#info").html(resWikiApi.query.pages[pageId].extract);
+  $("#city").html("<h2>").text(`${cityName}`);
+  $("#lang").html(countryName);
+  $("#lang").append(flagOfCountry, languageOfCountry, currencyOfCountry);
 }
 
 const button = $("#search").click(makeCalls);
