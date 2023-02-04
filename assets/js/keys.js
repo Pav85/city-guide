8dff1ae404960b053dd7f4198c510290836d843d
Pav85 <pawel.werbowy@gmail.com>
2023-02-04 12:32:13 +0000

    adds more js code

--- assets/js/keys.js
+++ assets/js/keys.js
@@ -1,2 +1,4 @@
 var omdbApiKey = "3k45j4k623j6k14";
 var openWeatherApiKey = "9E92A9EV239V29V82V2";
+
+var APIkey = "11f628af2243468c36a25b569b68d689"; // Pawel's API key
--- assets/js/project.js
+++ assets/js/project.js
@@ -1,1 +1,20 @@
 console.log('hello world');
+
+function cityWeather(city) {
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIkey + "&units=metric";
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+
+        var cityName = $("<h2>").text(moment().format("MM/DD/YYYY"));
+        var cityTemp = $("<p>").text("Temp: " + response.list[0].main.temp.toFixed(0) + " °C");
+        var cityWind = $("<p>").text("Wind: " + response.list[0].wind.speed + " KPH");
+        var cityHumidity = $("<p>").text("Humidity: " + response.list[0].main.humidity + "%");
+
+    })
+}
