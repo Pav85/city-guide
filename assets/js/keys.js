a588e9642fbb2d43c77e907069eccf9985937395
Alexander <110828403+PurAlex@users.noreply.github.com>
2023-02-06 12:03:47 +0000

    Update keys.js

--- assets/js/keys.js
+++ assets/js/keys.js
@@ -1,4 +1,4 @@
-var apiKey = "jykXqNRW0Y62n8S6SXW4C5ctkG3caX7l";
+var currencyApiKey = "jykXqNRW0Y62n8S6SXW4C5ctkG3caX7l";
 
 const config = {
     openWeatherApiKey: "9E92A9EV239V29V82V2",
@@ -6,4 +6,4 @@ const config = {
     xRapidAPIHost: "wft-geo-db.p.rapidapi.com",
 };
 
-var APIkey = "11f628af2243468c36a25b569b68d689"; // Pawel's API key
+var weatherAPIkey = "11f628af2243468c36a25b569b68d689"; // Pawel's API key
