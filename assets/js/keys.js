01cc6348c88f51ceccc50632333e7e5f15db4117
Olga Felczak <ofelczak@gmail.com>
2023-02-06 19:29:52 +0000

    Fix changes made after merges and refactoring

--- assets/js/keys.js
+++ assets/js/keys.js
@@ -1,9 +1,6 @@
-var currencyApiKey = "jykXqNRW0Y62n8S6SXW4C5ctkG3caX7l";
-
 const config = {
-    openWeatherApiKey: "9E92A9EV239V29V82V2",
-    xRapidAPIKey: "2870710a70mshb9dfd7b0cc1c38ap18047cjsn644f8c604a16",
-    xRapidAPIHost: "wft-geo-db.p.rapidapi.com",
+  xRapidAPIKey: "2870710a70mshb9dfd7b0cc1c38ap18047cjsn644f8c604a16",
+  xRapidAPIHost: "wft-geo-db.p.rapidapi.com",
+  currencyApiKey: "jykXqNRW0Y62n8S6SXW4C5ctkG3caX7l",
+  weatherApiKey: "11f628af2243468c36a25b569b68d689",
 };
-
-var weatherAPIkey = "11f628af2243468c36a25b569b68d689"; // Pawel's API key
